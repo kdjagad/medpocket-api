b64d6f3dca5bc4b624be9833e63d8b705d0d99e9
Keyur Jagad <kdjagad29@gmail.com>
2022-06-16 00:07:24 +0530

    added vercel json config

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -2,7 +2,7 @@ const { login, register, verify } = require('./user.controller');
 
 const router = require('express').Router();
 
-router.post('/', (req, res) => {
+router.get('/', (req, res) => {
     res.status(200).json({ "status": 1, "message": 'user fetched' });
 });
 router.post('/login', login);
