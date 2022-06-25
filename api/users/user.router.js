f4596da784221e16c8e54cde7a9b29ff39e47478
Keyur Jagad <kdjagad29@gmail.com>
2022-06-26 00:30:12 +0530

    changed user router

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -3,7 +3,7 @@ const { login, register, verify } = require('./user.controller');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({ "status": 1, "message": 'user fetched' });
+    res.status(200).json({ "status": 1, "message": 'user fetched 123' });
 });
 router.post('/login', login);
 router.post('/register', register);
