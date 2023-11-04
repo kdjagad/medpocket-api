4b5528f7aa680aa69fdc16518b54f160752c2332
Keyur Jagad <kdjagad29@gmail.com>
2023-11-04 15:53:34 +0530

    product search external added

--- api/products/product.router.js
+++ api/products/product.router.js
@@ -12,10 +12,13 @@ const {
 const router = require("express").Router();
 router.post("/search", verifyToken, searchProduct);
 router.post("/search-by-brand", verifyToken, searchProductByBrand);
+router.post("/external-search-by-brand", searchProductByBrand);
 router.get("/cart", verifyToken, getCart);
 router.delete("/cart/:cartId", verifyToken, deleteCart);
 router.post("/cart", verifyToken, addToCart);
 router.get("/:productId", verifyToken, getProductById);
+router.get("/external/:productId", getProductById);
 router.get("/content/:content", verifyToken, getProductContent);
+router.get("/external-content/:content", getProductContent);
 
 module.exports = router;
